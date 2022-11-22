9b21012f1e8421e57b58ef9455150b7ea14f1cfd
Jack Graymer <jackgraymer@gmail.com>
2022-11-22 14:10:20 +0100

    adds HTML interface, finishes fibonacci with for loop, creates recursion file

---
+++ Fibonacci/fibs.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/*
+Using iteration, write a function fibs which takes a number and returns an array containing that many numbers from the fibonacci sequence. Using an example input of 8, this method should return the array [0, 1, 1, 2, 3, 5, 8, 13].
+*/
+
+function fibs(n) {
+       
+let arr = [0,1]
+
+    for(let i=2; i<n; i++){
+        arr[i] = arr[i-1]+arr[i-2]
+    }
+    document.querySelector('.fibs').textContent += arr
+}
+
+
+fibs(8)
+
+
+
